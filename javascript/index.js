6068326fc420fca5a1d123eaa2f1d83a9efbd21f
saraphina1 <oyeyemibunmi21@gmail.com>
2022-12-11 19:34:22 +0100

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+alert("Hello")
+let userName = prompt("Enter your name");
+console.log(userName);
+
+let Age = prompt ("kindly enter your age");
+console.log(Age);
+
+alert("Welcome, " +userName+ " ,You are "+Age+ " years old.")
