298335d3dde7947d23e4cf52dd83617421afd127
saraphina1 <oyeyemibunmi21@gmail.com>
2022-12-17 21:24:40 +0100

    commit

---
+++ js practice/index.js
@@ -0,0 +1,17 @@
+let name = 'bunmi';
+console.log(name);
+let person = {
+    name : 'bunmi',
+    age : 30
+};
+console.log(person);
+
+let selectedcolors = ['red','blue'];
+selectedcolors[2] = 'green';
+selectedcolors[3] = 'peach';
+console.log(selectedcolors);
+
+let car = {benz:3,toyota:7,landcruiser:2};
+console.log(car["toyota"]);
+
+console.log(typeof true);
