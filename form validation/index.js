c54f0460ae30d36ada9ca298d79c1a23a909959e
saraphina1 <oyeyemibunmi21@gmail.com>
2022-12-18 08:19:29 +0100

    commit

--- form validation/index.js
+++ form validation/index.js
@@ -1,18 +1,18 @@
-const name = document.getElementById('name')
-const password = document.getElementById('password')
+const name = document.getElementById('uname')
+const password = document.getElementById('psw')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 
 form.addEventListener('submit', (e) => {
     let messages = []
-    if (name.value === '' || name.value == null){
+    if (uname.value === '' || uname.value == null){
         messages.push('name is required')
     }
     if (password.value.length <= 6) {
         messages.push('password must be longer than 6 characters')
     }
     else{
-        alert('Success')
+        alert('Login successful!')
     }
     
     if (messages.length > 0){
